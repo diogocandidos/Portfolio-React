fe9d2b5b92dc9fd2beca5cc341b5b010934631fb
Diogo Candido <diogocandido.s@gmail.com>
2020-11-18 12:58:38 -0800

    change repo and create new project

---
+++ portfolio/src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../Styles/Style.css';
+
+
+function Footer() {
+    return (
+        
+<div className="container">
+      <div className="text-center">
+        Copyright &copy; 2020 Diogo Candido da Silva
+      </div>
+    </div> 
+    )
+}
+
+export default Footer;
